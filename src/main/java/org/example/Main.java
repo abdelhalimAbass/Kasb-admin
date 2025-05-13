073a1c06760bb114cff555f9f2708c80e72f5c51
abdelhalimAbass <deboabas1@gmail.com>
2025-05-13 17:10:03 +0300

    fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,7 +16,7 @@ public class Main {
        driver.manage().window().maximize();
        signin(driver);
        creatingfund(driver);
-       driver.quit();
+       //driver.quit();
     }
     public static void signin (WebDriver driver){
         driver.get(global.url+"/login");
@@ -93,6 +93,23 @@ public class Main {
                 "<h1>Hello!</h1><p>This is rich content.</p>");
         // for send in performance fieled
         driver.findElement(By.id("translations_en_performance_conditions_inp")).sendKeys("this is performance");
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+//        try { Thread.sleep(2000); } catch (InterruptedException e) {}
+//
+//        // الوصول لعنصر input
+//        WebElement input1 = driver.findElement(By.xpath("//*[@id=\"terms_file_inp\"]"));
+//
+//        // التأكد من أنه ظاهر (لو مخفي)
+//        JavascriptExecutor js1 = (JavascriptExecutor) driver;
+//        js.executeScript("arguments[0].style.display = 'block';", input1);
+//
+//        // رفع الصورة
+//        input1.sendKeys("\"C:\\Users\\Masarat\\Desktop\\document33.pdf\"");
+//
+//        // تحقق (اختياري)
+//        String uploadedFileName1 = (String) js1.executeScript(
+//                "return arguments[0].files[0]?.name;", input1);
+//        System.out.println("Uploaded file: " + uploadedFileName1);
 
        // driver.findElement(By.xpath("//*[@id=\"submitted-form\"]/div[1]/div[2]/div/div/div[4]/div[1]/span[2]/span[1]/span")).click();
         //driver.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
@@ -103,17 +120,85 @@ public class Main {
 //        WebElement combobox=driver.findElement(By.xpath("//*[@id=\"submitted-form\"]/div[1]/div[2]/div/div/div[4]/div[1]/span[2]/span[1]/span"));
 //        Select select=new Select(combobox);
 //        select.selectByVisibleText("Value Capital");
-        WebElement dropdown = driver.findElement(By.xpath("//*[@id='submitted-form']/div[1]/div[2]/div/div/div[4]/div[1]/span[2]/span[1]/span"));
-        dropdown.click();
+
+//        WebElement dropdown = driver.findElement(By.xpath("//*[@id='submitted-form']/div[1]/div[2]/div/div/div[4]/div[1]/span[2]/span[1]/span"));
+//        dropdown.click();
+
         //Thread.sleep(100); // Replace with WebDriverWait in real test
+
         //driver.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
-        WebDriverWait d= new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement option= d.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"select2-authorized-person-sp-result-u5mj-1\"]")));
-        option.click();
-        option.click();
+    // try { Thread.sleep(2000); } catch (InterruptedException e) {}
+   //  driver.findElement(By.xpath("//*[@id=\"select2-authorized-person-sp-result-9o02-1\"]")).click();
+//        WebElement option=driver.findElement (By.xpath("//*[@id=\"select2-authorized-person-sp-result-5yki-1\"]"));
+//        option.click();
+//        WebElement drop= driver.findElement(By.xpath("//*[@id=\"submitted-form\"]/div[1]/div[2]/div/div/div[4]/div[2]/span[2]/span[1]/span"));
+//        drop.click();
+//        try { Thread.sleep(2000); } catch (InterruptedException e) {}
+//        WebElement optio=driver.findElement (By.xpath("///*[@id=\"submitted-form\"]/div[1]/div[2]/div/div/div[4]/div[2]/span[2]"));
+//        optio.click();
+
 
 //        WebElement option = driver.findElement(By.xpath(""));
 //        option.click();
+        global.wait_10(driver);
+        WebElement fileupload= driver.findElement(By.id("terms_file_inp"));
+        fileupload.sendKeys("C:\\Users\\Masarat\\Desktop\\document33.pdf");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement dropdwn = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id='submitted-form']/div[1]/div[2]/div/div/div[4]/div[1]/span[2]/span[1]/span")
+        ));
+        dropdwn.click();
+        System.out.println("i arrived before item");
+        WebElement option = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//li[contains(@class,'select2-results__option') and contains(text(),'Value Capital')]")
+        ));
+        System.out.println("i arrived before click");
+        option.click();
+        System.out.println("i arrived after click");
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        // choose currency
+        WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement dropdn = wait1.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id=\"submitted-form\"]/div[1]/div[2]/div/div/div[4]/div[2]/span[2]/span[1]/span")
+        ));
+        dropdn.click();
+        System.out.println("i arrived before item1");
+        WebElement opon = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//li[contains(text(),'SAR')]")
+        ));
+        System.out.println("i arrived before click1");
+        opon.click();
+        System.out.println("i arrived after click1");
+        //choose risk level
+        WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement dropd = wait2.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id=\"submitted-form\"]/div[1]/div[2]/div/div/div[4]/div[3]/span[2]/span[1]/span")
+        ));
+        dropd.click();
+        System.out.println("i arrived before item2");
+        WebElement opo= wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//li[contains(text(),'High')]")
+        ));
+        System.out.println("i arrived before click1");
+        opo.click();
+        System.out.println("i arrived after click1");
+        //choose fund type
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//*[@id=\"submitted-form\"]/div[1]/div[2]/div/div/div[5]/div[1]/span[2]/span[1]/span")).click();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//li[contains(text() , 'Public open ended')]")).click();
+        //select money transfare type
+        global.wait_10(driver);
+        driver.findElement(By.xpath("//*[@id=\"submitted-form\"]/div[1]/div[2]/div/div/div[5]/div[2]/span[2]/span[1]/span")).click();
+        System.out.println("i arrived here man");
+        global.wait_10(driver);
+        driver.findElement(By.xpath("//li[contains(text() , 'manual')]")).click();
+        global.wait_10(driver);
+        driver.findElement(By.xpath("//*[@id='bench_mark_inp']")).sendKeys("ghu");
+        //entring launch date
+        global.wait_10(driver);
+        driver.findElement(By.xpath("//*[@id='launch_date_inp']")).sendKeys("02/25/2025");
 
 
     }
--- src/main/java/org/example/global.java
+++ src/main/java/org/example/global.java
@@ -1,6 +1,13 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class global {
     public static String url="https://dashboard-kasb-beta.dafa.sa/admin";
     //WebDriver driver =new ChromeDriver();
+    public static void wait_10(WebDriver driver){
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
 }
